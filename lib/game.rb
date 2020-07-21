0aaea4b66cf4df44eff5dccddfa853c9c6e15309
pete3249 <katelynrpeterson16@gmail.com>
2020-07-20 22:32:25 -0400

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -29,11 +29,10 @@ class Game
     end 
 
     def won?
-        WIN_COMBINATIONS.any? do |winning_combo|
-           if @board.cells[winning_combo[0]] == @board.cells[winning_combo[1]] && 
-            @board.cells[winning_combo[1]] ==  @board.cells[winning_combo[2]] 
-                return winning_combo
-            end 
+        WIN_COMBINATIONS.detect do |winning_combo|
+            @board.cells[winning_combo[0]] == @board.cells[winning_combo[1]] && 
+            @board.cells[winning_combo[1]] ==  @board.cells[winning_combo[2]] &&
+            (@board.cells[winning_combo[0]] == "X" || @board.cells[winning_combo[0]] == "O")
         end 
     end 
 
@@ -54,9 +53,66 @@ class Game
     end 
 
     def winner
-      
+        array = won?
+        if array
+            @winner = @board.cells[array[0]]
+        end 
     end 
+
+    def turn
+        player_now = current_player
+        current_move = player_now.move(@board)
+        if @board.valid_move?(current_move)
+            @board.update(current_move, player_now)
+            @board.display
+        else
+            puts "Invalid"
+            turn
+        end 
+    end 
+  
 
 end 
 
 
+#     index = input_to_index(user_input)
+#       if valid_move?(index) == true
+#         letter = current_player
+#         move(index, letter)
+#         display_board
+#       else 
+#         turn
+#       end 
+#   end 
+
+
+
+#       game = Game.new
+#       allow($stdout).to receive(:puts)
+
+#       expect(game.player_1).to receive(:gets).and_return("1")
+
+#       game.turn
+#     end
+
+#     it 'asks for input again after a failed validation' do
+#       game = Game.new
+#       allow($stdout).to receive(:puts)
+
+#       expect(game.player_1).to receive(:gets).and_return("invalid")
+#       expect(game.player_1).to receive(:gets).and_return("1")
+
+#       game.turn
+#     end
+
+#     it 'changes to player 2 after the first turn' do
+#       game = Game.new
+#       allow($stdout).to receive(:puts)
+
+#       expect(game.player_1).to receive(:gets).and_return("1")
+#       expect(game.player_2).to receive(:gets).and_return("2")
+
+#       game.turn
+#       game.turn
+#     end
+#   end
